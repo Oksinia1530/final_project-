d92b38b2044261cb08eb3c76ac6253bfc805ca9c
«Oksana <averinaoy@gmail.com>
2023-03-11 22:44:47 +0500

    task

---
+++ task/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+
+string[] array1 = new string[7] {"6", "534","count", "897", "string", "new", "array"};
+string[] array2 = new string[array1.Length];
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void NewArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(array1, array2);
+NewArray(array2);
